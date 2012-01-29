8f319823be64f21122251673656f9efd0ac59855
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2012-01-28 17:20:42 -0800

    Implemented edit and update actions for tickets

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ActiveRecord::Base
-  has_many :tickets
-  validates :name, :presence => true 
+  
+  has_many :tickets, :dependent => :delete_all
+  
+  validates :name, :presence => true
+  
 end
