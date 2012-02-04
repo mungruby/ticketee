0e9de43b76d06a31c358180e4c2aa3be36b202aa
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2012-02-04 12:00:33 -0800

    Made projects only visible to users with permission to see them

--- features/viewing_tickets.feature
+++ features/viewing_tickets.feature
@@ -4,17 +4,20 @@ Feature: Viewing tickets
   I want to see them on that project's page
 
   Background:
-  Given there are the following users:
-    | email             | password |
-    | user@ticketee.com | password |
-    And there is a project called "TextMate 2"
+    Given there are the following users:
+      | email             | password |
+      | user@ticketee.com | password |
+    And I am signed in as them
+    Given there is a project called "TextMate 2"
+    And "user@ticketee.com" can view the "TextMate 2" project
 	And "user@ticketee.com" has created a ticket for this project:
-	    | title           | description                   |
-	    |  Make it shiny! | Gradients! Starbursts! Oh my! |
+	  | title           | description                   |
+	  |  Make it shiny! | Gradients! Starbursts! Oh my! |
 	And there is a project called "Internet Explorer"
+	And "user@ticketee.com" can view the "Internet Explorer" project
 	And "user@ticketee.com" has created a ticket for this project:
-	    | title                | description   |
-	    | Standards compliance | Isn't a joke. |
+	  | title                | description   |
+	  | Standards compliance | Isn't a joke. |
 	And I am on the homepage
 
   Scenario: Viewing tickets for a given project
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -7,25 +7,34 @@ describe ProjectsController do
   let(:project) { Factory :project }
   
   context "a standard user" do
-    { "new" => "get",
-      "create" => "post",
-      "edit" => "get",
-      "update" => "put",
-      "destroy" => "delete" }.each do |action, method|
+    
+    it "cannot access the show action" do
+      sign_in :user, user
+      get :show, :id => project.id
+      response.should redirect_to projects_path
+      flash[:alert].should eql 'The project you were looking for could not be found.'
+    end
+        
+    { 'new' => 'get',
+      'create' => 'post',
+      'edit' => 'get',
+      'update' => 'put',
+      'destroy' => 'delete' }.each do |action, method|
         
       it "cannot access the #{action} action" do
-        sign_in(:user, user)
+        sign_in :user, user
         send(method, action.dup, :id => project.id)
-        response.should redirect_to(root_path)
-        flash[:alert].should eql("You must be an admin to do that.")
+        response.should redirect_to root_path
+        flash[:alert].should eql 'You must be an admin to do that.'
       end
     end
   end
     
   it "displays an error for a missing project" do
-    get :show, :id => "not-here"
-    response.should redirect_to(projects_path)
-    message = "The project you were looking for could not be found."
-    flash[:alert].should eql(message)
+    sign_in :user, user
+    get :show, :id => 'not-here'
+    response.should redirect_to projects_path
+    message = 'The project you were looking for could not be found.'
+    flash[:alert].should eql message
   end
 end
