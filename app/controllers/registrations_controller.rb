07c92b3ef99d8af6d3be448f2b614517bfbc3795
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2012-02-04 13:16:03 -0800

    Fix signing up feature, take user to alternative confirmation page

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,14 @@
+class RegistrationsController < Devise::RegistrationsController
+  # self.scoped_views = false
+  
+  # def new
+  #   super
+  # end
+
+  protected
+  # private
+    
+  def after_inactive_sign_up_path_for(resource)
+    confirm_user_path
+  end
+end
