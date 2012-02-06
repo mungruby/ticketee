86accca5c00f60edd6c354b58c1f3fd569965d55
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2012-02-06 11:59:57 -0800

    Restrict destroy action to users with permission.

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -5,7 +5,7 @@ class TicketsController < ApplicationController
   before_filter :find_ticket, :only => [:show, :edit, :update, :destroy]
   before_filter :authorize_create!, :only => [:new, :create]
   before_filter :authorize_update!, :only => [:edit, :update]
-  # before_filter :authorize_delete!, :only => [:destroy]
+  before_filter :authorize_delete!, :only => :destroy
   
   def new
     @ticket = @project.tickets.build
