5dbbe6d726ce8a701995151c33b4f9788c2beca3
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2012-02-08 17:43:34 -0800

    Assets are now strictly served through FilesController

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,6 @@
 class Asset < ActiveRecord::Base
-  has_attached_file :asset
+  belongs_to :ticket
+  has_attached_file :asset, :path => (Rails.root + 'files/:id').to_s,
+                            :url  => 'files/:id'
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -65,6 +65,8 @@ Ticketee::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
 
+  resources :files
+
   namespace :admin do
     root :to => 'base#index'
     resources :users do
