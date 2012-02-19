93327f0d255b85d4d45b3b44ad31bcb29347c4fc
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2012-02-19 10:36:50 -0800

    Add email ticket notifications

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -16,6 +16,10 @@ class Ticket < ActiveRecord::Base
   
   has_and_belongs_to_many :tags
   
+  has_and_belongs_to_many :watchers, join_table: 'ticket_watchers', class_name: 'User'
+  
+  after_create :creator_watches_me
+  
   validates :title, :presence => true
   validates :description, :presence => true, :length => {:minimum => 10}  
 
@@ -25,4 +29,11 @@ class Ticket < ActiveRecord::Base
     end
     self.tags << tags
   end
+
+
+  private
+  
+    def creator_watches_me
+      self.watchers << user
+    end
 end
