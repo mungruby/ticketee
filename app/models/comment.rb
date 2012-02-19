e8d5cba805392ff7c61c7787d539de1564ed3f00
R. Scott Shaffer <r_scott.shaffer@comcast.net>
2012-02-19 15:49:02 -0800

    Users are automatically subscribed to a ticket when they comment on it.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
   
   before_create :set_previous_state
   after_create :set_ticket_state
-  # after_create :creator_watches_ticket
+  after_create :creator_watches_ticket
 
   belongs_to :user
   belongs_to :state
@@ -11,7 +11,7 @@ class Comment < ActiveRecord::Base
   belongs_to :previous_state, :class_name => "State"
 
   delegate :project, :to => :ticket
-
+  
   validates :text, :presence => true
 
   private
